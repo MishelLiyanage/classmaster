9adc71927c5760c1f5b8709a67efaf0bf40272d7
DinalieLiyanage <bhagyafdo97@gmail.com>
2024-02-26 22:59:11 +0530

    Add student payment UI

---
+++ src/classmaster/models/CourseStudentPaymentDto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classmaster.models;
+
+/**
+ *
+ * @author Siri
+ */
+public class CourseStudentPaymentDto extends CourseStudentPayment{
+    
+    private String courseName;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    
+    
+    
+}
